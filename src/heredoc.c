919b006191d887b484eec19c9e028c52feccef01
gufestin <gufestin@student.42.fr>
2022-06-23 15:03:40 +0200

    heredoc non fini

--- src/heredoc.c
+++ src/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: gufestin <gufestin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/22 23:56:04 by gufestin          #+#    #+#             */
-/*   Updated: 2022/06/23 01:57:52 by gufestin         ###   ########.fr       */
+/*   Updated: 2022/06/23 14:59:46 by gufestin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,12 +32,20 @@ int	ft_heredoc(char *eof)
 		exit(1); // open error
 	eof_len = ft_strlen(eof);
 	read = readline("> ");
+	if (read)
+	{
 	while (ft_strncmp(read, eof, eof_len))
 	{
 		ft_putstr_fd(read, fd);
 		ft_putstr_fd("\n", fd);
 		free(read);
 		read = readline("> ");
+		if (!read)
+		{
+			printf("test\n");
+			break ;
+		}
+	}
 	}
 	free(read);
 	close(fd);

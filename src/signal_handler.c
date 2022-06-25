3a4e53347d3daa7c6e70c1eb9f8085794bc52565
Isabelle REN <iren@e2r10p13.clusters.42paris.fr>
2022-06-25 05:08:10 +0200

    fix on STDOUT / STDIN

--- src/signal_handler.c
+++ src/signal_handler.c
@@ -6,7 +6,7 @@
 /*   By: iren <iren@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/19 20:21:24 by iren              #+#    #+#             */
-/*   Updated: 2022/06/25 04:32:43 by iren             ###   ########.fr       */
+/*   Updated: 2022/06/25 05:07:32 by iren             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	sig_prompt(int signum)
 	if (signum == SIGINT)
 	{
 		g_errno = 130;
-		ft_putendl_fd("", STDIN);
+		ft_putendl_fd("", STDOUT);
 		rl_on_new_line();
 		rl_replace_line("", 0);
 		rl_redisplay();
@@ -29,7 +29,7 @@ void	sig_prompt_child(int signum)
 	if (signum == SIGINT)
 	{
 		g_errno = 130;
-		ft_putendl_fd("", STDIN);
+		ft_putendl_fd("", STDOUT);
 	}
 }
 
